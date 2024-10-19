f77aa87c54b26978f76ead00ae5c4d4e9eb83f99
srividya-sundaravadivelu <srividya-sundaravadivelu>
2024-10-19 16:05:07 -0500

    merged changes

--- src/test/java/infrastructure/Setup.java
+++ src/test/java/infrastructure/Setup.java
@@ -1,24 +1,62 @@
+//package infrastructure;
+//
+//import java.time.Duration;
+//
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//import org.openqa.selenium.chrome.ChromeOptions;
+//
+//import io.cucumber.java.Before;
+//
+//
+//public class Setup {
+//
+//	public static WebDriver driver;
+//
+//	@Before
+//	public void setWebDriver() {
+//
+//		ChromeOptions chromeOptions = new ChromeOptions();
+//		chromeOptions.addArguments("['start-maximized']");
+//		driver = new ChromeDriver(chromeOptions);
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+//
+//	}
+//}
+
 package infrastructure;
-
 import java.time.Duration;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
-import io.cucumber.java.Before;
 
 public class Setup {
 
 	public static WebDriver driver;
 
-	@Before
-	public void setWebDriver() {
+	
+	public WebDriver setWebDriver() {
 
-		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("['start-maximized']");
-		driver = new ChromeDriver(chromeOptions);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+		
+		WebDriverManager.edgedriver().setup();
+		driver = new EdgeDriver();
+		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		return driver;
+		
+	}
+	
+	public static WebDriver getdriver() {
+		return driver;
 
 	}
+
+	public void closeallDriver() {
+		driver.close();
+	}
+
+
+	
+
 }
--- src/test/java/infrastructure/TearDown.java
+++ src/test/java/infrastructure/TearDown.java
@@ -1,22 +1,22 @@
-package infrastructure;
-
-import org.openqa.selenium.WebDriver;
-
-import io.cucumber.java.After;
-import io.cucumber.java.Scenario;
-
-public class TearDown {
-
-	private WebDriver driver;
-
-	public TearDown() {
-		this.driver = Setup.driver;
-	}
-
-	@After
-	public void quitDriver(Scenario scenario) {
-
-		this.driver.quit();
-	}
-
-}
+//package infrastructure;
+//
+//import org.openqa.selenium.WebDriver;
+//
+//import io.cucumber.java.After;
+//import io.cucumber.java.Scenario;
+//
+//public class TearDown {
+//
+//	private WebDriver driver;
+//
+//	public TearDown() {
+//		this.driver = Setup.driver;
+//	}
+//
+//	@After
+//	public void quitDriver(Scenario scenario) {
+//
+//		this.driver.quit();
+//	}
+//
+//}
